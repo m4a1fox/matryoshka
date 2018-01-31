02647bee2c0591264074b9e7e1b30eb37aa8b0e2
m4a1fox <bogdanovmaxim87@gmail.com>
2018-01-31 15:08:37 +0300

    Fix Cacheable to use string OR object

--- src/Cacheable.php
+++ src/Cacheable.php
@@ -11,6 +11,6 @@ trait Cacheable
      */
     public function getCacheKey()
     {
-        return sprintf("%s/%s-%s", get_class($this), $this->id, $this->updated_at->timestamp);
+        return sprintf("%s/%s-%s", get_class($this), $this->id, is_string($this->updated_at) ? $this->updated_at : $this->updated_at->timestamp);
     }
 }
