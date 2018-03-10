ba168c9e598bdfdeb48040684c00d622f6c12fc0
m4a1fox <bogdanovmaxim87@gmail.com>
2018-03-10 16:05:02 +0300

    Add default flushing a view if APP_ENV is local

--- src/FlushViews.php
+++ src/FlushViews.php
@@ -8,9 +8,7 @@ class FlushViews
 {
     public function handle($request, $next)
     {
-        if (app()->environment() === 'local') {
-            Cache::tags('view')->flush();
-        }
+        Cache::tags('view')->flush();
 
         return $next($request);
     }
--- src/MatryoshkaServiceProvider.php
+++ src/MatryoshkaServiceProvider.php
@@ -4,16 +4,21 @@ namespace Matryoshka;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Http\Kernel;
 
 class MatryoshkaServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
      *
-     * @return void
+     * @param Kernel $kernel
      */
-    public function boot()
+    public function boot(Kernel $kernel)
     {
+        if ($this->app->environment() === 'local') {
+            $kernel->pushMiddleware('Matryoshka\FlushViews');
+        }
+
         Blade::directive('cache', function ($expression) {
             return "<?php if (! app('Matryoshka\BladeDirective')->setUp({$expression})) { ?>";
         });
@@ -30,10 +35,6 @@ class MatryoshkaServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(BladeDirective::class, function () {
-            return new BladeDirective(
-                new RussianCaching(app('cache.store'))
-            );
-        });
+        $this->app->singleton(BladeDirective::class);
     }
 }
